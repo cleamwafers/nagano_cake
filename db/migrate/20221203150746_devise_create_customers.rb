93625448d30b1d3070c1162836cf3a3c97b3230a
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-12-07 06:05:36 +0000

    make controller public/items

--- db/migrate/20221203150746_devise_create_customers.rb
+++ db/migrate/20221203150746_devise_create_customers.rb
@@ -36,14 +36,10 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name_kana
       t.string :first_name_kana
-      t.string :email
-      t.string :encrypted_password
       t.string :postal_code
       t.string :address
       t.string :telephone_number
       t.boolean :is_deleted, null: false, default: false
-      t.datetime :created_at
-      t.datetime :updated_at
       t.timestamps null: false
     end
 
