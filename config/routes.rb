77a6c8c00641402f13fbb73754197643beb6e9d5
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-12-10 12:56:18 +0000

    config/routes 見直し

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
 
     # 管理者用
     # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-   sessions: "admin/sessions"
+   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
    }
-   namespace :admin do
-   get 'top'=>'homes#top'
-   resources :products,only: [:index,:new,:create,:show,:edit,:update,]
-   resources :genres,only: [:index,:create,:edit,:update]
-   resources :customers,only: [:index,:show,:edit,:update]
-   resources :order_items, only: [:update]
-   resources :orders,only: [:show,:update] do
+  
+  namespace :admin do
+    get 'top'=>'homes#top'
+    resources :products,only: [:index,:new,:create,:show,:edit,:update,]
+    resources :genres,only: [:index,:create,:edit,:update]
+    resources :customers,only: [:index,:show,:edit,:update]
+    resources :order_items, only: [:update]
+    resources :orders,only: [:show,:update] do
   	  member do
         get :current_index
         resource :order_details,only: [:update]
@@ -22,37 +23,38 @@ Rails.application.routes.draw do
     end
   end
 
- devise_for :public,skip: [:passwords], controllers: {
-     registrations: "public/registrations",
-     sessions: 'public/sessions'
-    }
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
   namespace :public do
     get 'top' =>'homes#top'
     get 'homes/about'
       # 顧客用
       # URL /customers/sign_in ...
-    resources :items,only[:index,:show]
+    resources :items,only: [:index,:show]
 
    	resources :customers,only: [:show,:edit,:update] do
-  		 collection do
+  		collection do
   	     get 'quit'
   	     patch 'out'
   	  end
-  	 end
+  	end
 
     resources :cart_items,only: [:index,:update,:create,:destroy] do
-        collection do
-          delete '/' => 'cart_items#all_destroy'
-        end
+      collection do
+        delete '/' => 'cart_items#all_destroy'
       end
+    end
 
     resources :orders,only: [:new,:index,:show,:create] do
-        collection do
-          post 'comfirm'
-          get 'thanx'
-        end
+      collection do
+        post 'comfirm'
+        get 'thanx'
       end
-
-      resources :addresses,only: [:index,:create,:edit,:update,:destroy]
     end
+
+    resources :addresses,only: [:index,:create,:edit,:update,:destroy]
   end
+end
