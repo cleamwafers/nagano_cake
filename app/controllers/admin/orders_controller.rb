c3a7c45fb746acd0d7df1635973e925657c32c34
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-12-23 02:10:43 +0000

    admin/top.html.erb 注文履歴一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,5 +10,12 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    @order = Order.find(params[:id])
+		if @order.update(order_params)
+		   flash[:success] = "注文ステータスを変更しました"
+		   redirect_to admin_order_path(@order)
+		else
+		   render "show"
+		end
   end
 end
