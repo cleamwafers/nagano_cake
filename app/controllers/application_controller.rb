c72058effc4dd4fcb993582ea41d11966ec62dd8
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-01-08 14:37:37 +0000

    publicログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+ before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(last_name, first_name, last_name_kana, first_name_kana, postal_code, address, telephone_number, password_confirmation))
+  end
 end
