a9250f05b3d587fec751ffe101b2b1977eea10fa
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-01-17 01:45:33 +0000

    orders/comfirm.html.erb

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -20,24 +20,6 @@ class Public::CartItemsController < ApplicationController
     cart_item = Cart_items.find(params_cart_item)
     cart_item.destroy
     redirect_to  public_cart_items_path
-
-
-    #   # カートの中に同じ商品が重複しないようにして　古い商品と新しい商品の数量を合わせる
-    # @update_cart_item =  CartItem.find_by(item_id: )
-    # if @update_cart_item.present? && @cart_item.valid?
-    #     @cart_item.amount += @update_cart_item.amount
-    #     @update_cart_item.destroy
-    # end
-
-    # if @cart_item.save
-    #   flash[:notice] = "#{@cart_item.item.name}をカートに追加しました"
-    #   redirect_to items_path
-    # else
-    #   @item = Item.find(params[:cart_item][:product_id])
-    #   @cart_item = CartItem.new
-    #   flash[:alert] = "個数を選択してください"
-    #   render ("customer/items/show")
-    # end
   end
 
   def all_destroy
@@ -50,7 +32,7 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
 
-      # カートの中に同じ商品が重複しないようにして　古い商品と新しい商品の数量を合わせる
+      # カートの中に同じ商品が重複しないようにして 古い商品と新しい商品の数量を合わせる
     if @update_cart_item = current_customer.cart_items.find_by(item: @cart_item.item)
      @update_cart_item.update(amount: @update_cart_item.amount + @cart_item.amount)
      redirect_to public_cart_items_path
@@ -58,8 +40,7 @@ class Public::CartItemsController < ApplicationController
     else
      @cart_item.save
      redirect_to public_cart_items_path
-   end
-
+    end
   end
 
   private
