60d82b91e61132e27b6c73568e8e0997c3e5ad13
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-01-19 13:02:39 +0000

    配送先登録一覧

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,7 +2,7 @@ class Public::AddressesController < ApplicationController
 before_action :authenticate_customer!
 
   def index
-    @addresses = current_customer.address
+    @addresses = current_customer.addresses
   	@address = Address.new
   end
 
