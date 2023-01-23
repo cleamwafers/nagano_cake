c2a694443d3d14eb3ae994bbd178dbd5011e682b
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-01-23 04:51:42 +0000

    注文情報確認画面　残りレイアウト

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,13 +6,29 @@ class Public::OrdersController < ApplicationController
   end
 
   def comfirm
-  @cart_items =CartItem.all
-  @order = Order.new(order_params)
-  @total_price = 0
-  @order.shopping_fee =800
-  @order.postal_code = current_customer.postal_code
-  @order.address = current_customer.address
-  @order.name = current_customer.first_name + current_customer.last_name
+
+    @cart_items =CartItem.all
+    @order = Order.new(order_params)
+    @total_price = 0
+    @order.shopping_fee = 800
+   if params[:order][:adresses_method] == "0"
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+   elsif params[:order][:adresses_method]== "1"
+    @order = Order.new(order_params)
+    @order.shopping_fee = 800
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+   else params[:order][:adresses_method] == "2"
+    @order = Order.new(order_params)
+    @order.shopping_fee = 800
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+   end
   end
 
   def thanx
