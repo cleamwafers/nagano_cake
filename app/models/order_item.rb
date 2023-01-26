bf3e37906f9cb769b73eb123e534fcf4ee2f24b5
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-01-26 04:39:50 +0000

    admin/items/edit.html.erb 変更保存

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,9 @@ class OrderItem < ApplicationRecord
 belongs_to :item
 belongs_to :order
   enum production_status: {cannot_be_manufactured: 0, waiting_for_production: 1, in_production: 2, finished: 3}
+
+  def subtotal
+    purchase_price * quantity
+  end
 end
+
