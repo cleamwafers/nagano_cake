4d7acee4c48a37573578a2b789f98cdb34256ab8
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-01-28 14:59:31 +0000

    注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
   def show
-    @orders = Order.page(params[:page])
+    @order = Order.find(params[:id])
   end
 
   def update
